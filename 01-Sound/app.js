cdea69112d34b974f9bc085789e570839f80e6f6
3ndrius <andrzejo1991@gmail.com>
2018-06-01 11:01:54 +0200

    v3

--- 01-Sound/app.js
+++ 01-Sound/app.js
@@ -5,7 +5,7 @@ window.addEventListener('keydown', (e) => {
       const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     console.log(e);
     if(!audio) return;
-
+    audio.currentTime = 0;
     audio.play();
 });
 
